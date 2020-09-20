7bfb412a54a8d2cab3f59d725dbec9f319805f2b
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-09-20 15:19:58 +0900

    countries#index作成

---
+++ app/controllers/countries_controller.rb
@@ -0,0 +1,18 @@
+class CountriesController < ApplicationController
+  def index
+    @articles = Article.all
+    @asia_countries = Country.find_all_by_region "Asia"
+    @europe_countries = Country.find_all_by_region "Europe"
+    @north_america_countries = Country.find_all_by_region "North America"
+    @south_america_countries = Country.find_all_by_region "South America"
+    @central_america_countries = Country.find_all_by_region "Central America"
+    @oceania_countries = Country.find_all_by_region "Oceania"
+    @africa_countries = Country.find_all_by_region "Africa"
+    @middle_east_countries = Country.find_all_by_region "Middle East"
+  end
+
+  def show
+    @article = Article.find_by_country_id(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :countries, only: [:index, :show]
   resources :articles
-  root to: 'articles#index'
+  root to: 'countries#index'
 end
